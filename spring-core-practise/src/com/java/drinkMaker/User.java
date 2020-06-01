6f3290fb1693c9d9bb4a765311ad85da24b9b268
srimuthurajesh <srimuthurajesh@gmail.com>
2020-06-01 17:44:28 +0530

    java approach

--- spring-core-practise/src/com/java/drinkMaker/User.java
+++ spring-core-practise/src/com/java/drinkMaker/User.java
@@ -1,5 +1,10 @@
 package com.java.drinkMaker;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -14,6 +19,19 @@ public class User {
 		sugar = app.getBean("sugarFromBean",Sugar.class);	
 		System.out.println(sugar.addSuger());
 		System.out.println(sugar.getSugarCane().addSugarCaneJuice());
+		
+		try{  
+			Class.forName("org.h2.Driver");  
+			Connection con=DriverManager.getConnection(  
+			"jdbc:h2:~/test", "sa","sa");  
+
+			Statement stmt=con.createStatement();  
+			ResultSet rs=stmt.executeQuery("select * from emp");  
+			while(rs.next())  
+			System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+			con.close();  
+			}
+		catch(Exception e){ System.out.println(e);}  
 		
 	}
 }
